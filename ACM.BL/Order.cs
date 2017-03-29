a3d57cabe20d0c823e98b8b58a1253794e4a9db4
yhlong0 <yhlong2@gmail.com>
2017-03-29 16:23:06 -0500

    Add OrderItem and Order class

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -8,7 +8,38 @@ namespace ACM.BL
 {
     class Order
     {
+        public Order()
+        {
+            
+        }
+
+        public Order(int orderId)
+        {
+            this.OrderId = orderId;
+        }
+
+        public DateTimeOffset? OrderDate { get; set; }
+
         public int OrderId { get; private set; }
 
+        public Order Retrieve(int orderId)
+        {
+            return new Order();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+            if (OrderDate == null)
+            {
+                isValid = false;
+            }
+            return isValid;
+        }
     }
 }
---
+++ ACM.BL/OrderItem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class OrderItem
+    {
+        public OrderItem()
+        {
+            
+        }
+
+        public OrderItem(int orderItemId)
+        {
+            this.OrderItemId = orderItemId;
+        }
+
+        public int OrderItemId { get; private set; }
+        public int OrderQuantity { get; set; }
+        public int ProductId { get; set; }
+        public decimal? PurchasePrice { get; set; }
+
+        public OrderItem Retrieve(int orderItemId)
+        {
+            return new OrderItem();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            bool isValid = true;
+
+            if (OrderQuantity <= 0)
+            {
+                isValid = false;
+            }
+
+            if (ProductId <= 0)
+            {
+                isValid = false;
+            }
+
+            if (PurchasePrice == null)
+            {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    }
+}
