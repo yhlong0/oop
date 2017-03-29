faf3e24084614eee1d482704362fa8769caa4d25
yhlong0 <yhlong2@gmail.com>
2017-03-28 23:06:01 -0500

    Add Product Class

---
+++ ACM.BL/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ACM.BL
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(int productId)
+        {
+            this.ProductId = productId;
+        }
+
+        public Decimal? CurrentPrice { get; set; }
+
+        public int ProductId { get; private set; }
+        public string ProductDescription { get; set; }
+        public string ProductName { get; set; }
+
+        public Product Retrieve(int productId)
+        {
+            return new Product();
+        }
+
+        public List<Product> Retrieve()
+        {
+            return new List<Product>();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool validate()
+        {
+            bool isValid = true;
+
+            if (string.IsNullOrWhiteSpace(ProductName))
+            {
+                isValid = false;
+            }
+
+            if (CurrentPrice == null)
+            {
+                isValid = false;
+            }
+            return isValid;
+        }
+    }
+}
